535b41b34fc013c7b0fc5e148960ad854395a235
Afshawn Lotfi <me@afshawn.org>
2023-07-08 03:38:15 +0000

    Added su2 custom executable path

--- paraflow/simulation/su2.py
+++ paraflow/simulation/su2.py
@@ -99,19 +99,24 @@ def run_su2_simulation(
     version: str = DEFAULT_VERSION,
     is_mpi: bool = False,
     custom_download_url: Optional[str] = None,
+    custom_executable_path: Optional[str] = None
 ):
     num_zones=len(meshes)
 
     platform = get_platform()
-    if custom_download_url:
-        url = custom_download_url
-    else:
-        url = f"https://github.com/su2code/SU2/releases/download/v{version}/SU2-v{version}-{platform}64{'-mpi' if is_mpi else ''}.zip"
-    executable_name = url.split("/")[-1].replace(".zip", "")
-    executable_path = f"{install_dir}/{executable_name}"
     
-    if not os.path.exists(executable_path):
-        install_su2(url, install_dir, executable_name)
+    if custom_executable_path:
+        executable_path = custom_executable_path
+    else:
+        if custom_download_url:
+            url = custom_download_url
+        else:
+            url = f"https://github.com/su2code/SU2/releases/download/v{version}/SU2-v{version}-{platform}64{'-mpi' if is_mpi else ''}.zip"
+        executable_name = url.split("/")[-1].replace(".zip", "")
+        executable_path = f"{install_dir}/{executable_name}"
+    
+        if not os.path.exists(executable_path):
+            install_su2(url, install_dir, executable_name)
 
     print(f"Setting up SU2 Simulation for {config_path}")
     setup_su2_simulation(meshes, config, config_path)
